e318413139bf2cd1d2e9453b791066dbcb80cf3c
inabajunmr <inabajun.for.regi@gmail.com>
2018-09-29 19:17:01 +0900

    add package

---
+++ kevaf/kevaf.go
@@ -0,0 +1,15 @@
+package kevaf
+
+type FileMap struct {
+	path string
+}
+
+func (f FileMap) Put(key string, value []byte) error {
+	// TOOD
+	return nil
+}
+
+func (f FileMap) Get(key string) (value []byte, err error) {
+	// TODO
+	return nil, nil
+}
