36d225f64cafea1f4c0878375c37ab6b0159a3b7
inabajunmr <inabajun.for.regi@gmail.com>
2018-09-30 21:44:04 +0900

    initial

--- lib/kevaf.go
+++ lib/kevaf.go
@@ -1,15 +1,32 @@
+/*
+Package kevaf is light-weight filebase kvs.
+*/
 package kevaf
 
+import "os"
+
+/*
+FileMap is file base Kvs interface
+*/
 type FileMap struct {
 	path string
 }
 
+/*
+Put create file by filename as key and content as value
+When failed to write file, return error
+*/
 func (f FileMap) Put(key string, value []byte) error {
-	// TOOD
+	file, err := os.Create(f.path)
+	defer file.Close()
+	if err != nil {
+		return err
+	}
+
+	file.Write(value)
 	return nil
 }
 
 func (f FileMap) Get(key string) (value []byte, err error) {
-	// TODO
 	return nil, nil
 }
